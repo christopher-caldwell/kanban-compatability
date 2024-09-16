c6c62203ee1db7094677702507016cbd855ee39d
christopher-caldwell <well.done.514@gmail.com>
2024-09-16 13:28:38 -0500

    fix: Removing source, as it's not needed for demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,10 @@ import { FC } from 'react'
 import { UncontrolledBoard } from '@caldwell619/react-kanban'
 
 import { board } from './data'
-import { Source } from './Source'
 
 export const App: FC = () => {
   return (
     <body>
-      <Source
-        title="Uncontrolled"
-        url="https://github.com/christopher-caldwell/react-kanban/blob/main/demo/src/features/uncontrolled/index.tsx"
-      />
       <UncontrolledBoard
         initialBoard={board}
         onCardRemove={({ board, card, column }) => {
